b17713cf2438ecbe272d6d017386889e81158cc3
LinKitten <2634024527@qq.com>
2023-07-06 09:24:37 +0800

    底部标签栏

---
+++ src/components/common/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="footerBox">
+        <div class="footer">
+            <van-tabbar route>
+                <van-tabbar-item replace to="/" icon="wap-home-o">首页</van-tabbar-item>
+                <van-tabbar-item replace to="/typelist" icon="apps-o">分类</van-tabbar-item>
+                <van-tabbar-item replace to="/" icon="cart-o">购物车</van-tabbar-item>
+                <van-tabbar-item replace to="/business/index" icon="manager-o">我的</van-tabbar-item>
+            </van-tabbar>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
